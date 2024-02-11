9dffc4defebbbacd12527accc560a4cd87cdb439
Tamara Garcia <tamara.garciamarcos@educa.madrid.org>
2024-02-11 14:50:50 +0100

    utlimo commit por siaca

--- Tamara_GarciaMarcos_practica1_2_aad_Recu/src/main/java/PatronDAO/DAO_Generico.java
+++ Tamara_GarciaMarcos_practica1_2_aad_Recu/src/main/java/PatronDAO/DAO_Generico.java
@@ -1,16 +1,15 @@
 package PatronDAO;
 
-import java.sql.SQLException;
 import java.util.List;
 /**
  * Esta interfaz hace la funcion de DAO generico para todas las clases
  * @author Tamara Garcia Marcos
  */
 public interface DAO_Generico<T> {
-	void insert(T object) throws SQLException;
+	void insert(T object);
 
-	void delete(T object) throws SQLException;
+	void delete(T object);
 
-	List<T> getAll() throws SQLException;
-	// Otros métodos generales si es necesario
+	List<T> getAll();
+	
 }
